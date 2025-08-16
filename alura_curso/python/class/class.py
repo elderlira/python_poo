4a84ee8a86113de094be755377d3f00af611d51b
elder lira <elderfranca@hotmail.com>
2025-08-16 17:11:33 -0300

    first commit

---
+++ alura_curso/python/class/class.py
@@ -0,0 +1,6 @@
+class Musics:
+    
+    def __init__(self, nome: str, artista: str, duracao: int):
+        self.nome: str = nome
+        self.artista: str = artista
+        self.duracao: int = duracao
---
+++ alura_curso/python/class/exercise_class.py
@@ -0,0 +1,37 @@
+# Exercícios
+# Implemente uma classe chamada Carro com os atributos básicos, como modelo, cor e ano. 
+# Crie uma instância dessa classe e atribua valores aos seus atributos.
+# Crie uma classe chamada Restaurante com os atributos nome, categoria, ativo e crie mais 2 atributos. 
+# Instancie um restaurante e atribua valores aos seus atributos.
+# Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros e inicia ativo como False por padrão. 
+# Crie uma instância utilizando o construtor.
+# Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância, seja exibida uma mensagem formatada com o nome e a categoria.
+# Exiba essa mensagem para uma instância de restaurante.
+# Crie uma classe chamada Cliente e pense em 4 atributos. 
+# Em seguida, instancie 3 objetos desta classe e atribua valores aos seus atributos através de um método construtor.
+
+class Carro:
+
+    def __init__(self, modelo: str, cor: str, ano: int):
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+
+carro1 = Carro("Fiesta", "prata", 2013)
+
+class Restaurante:
+    def __init__(self, nome, categoria, local, atendimento_online, ativo=False):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = ativo
+        self.local = local
+        self.atendimento_online = atendimento_online
+
+    def __str__(self):
+        return f"O restaurante {self.nome} é do tipo {self.categoria}"
+
+pizzaria = Restaurante(nome = 'Noustra pizza', categoria='pizzaria italiana', local='Praca nossa senhora da luz', ativo=True)
+print(pizzaria)
+
+
+
